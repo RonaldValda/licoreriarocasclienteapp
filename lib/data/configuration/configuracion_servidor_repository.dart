abcb8798d7b2368a54e691a8af728943cdf444ac
FranzReynaldo98 <rhyno12091991@gmail.com>
2022-03-14 15:06:15 -0400

    cambio de servidor

--- lib/data/configuration/configuracion_servidor_repository.dart
+++ lib/data/configuration/configuracion_servidor_repository.dart
@@ -1,7 +1,7 @@
 import 'package:graphql_flutter/graphql_flutter.dart';
 class GraphQLConfiguration{
-  final HttpLink httpLink=new HttpLink("https://licoreriarocasapp.herokuapp.com/");
-  final WebSocketLink webSocketLink=WebSocketLink("ws://licoreriarocasapp.herokuapp.com/");
+  final HttpLink httpLink=new HttpLink("https://licoreriarocasapp-v2.herokuapp.com/");
+  final WebSocketLink webSocketLink=WebSocketLink("ws://licoreriarocasapp-v2.herokuapp.com/");
   //final HttpLink httpLink=new HttpLink("http://192.168.100.93:4000/");
   //final WebSocketLink webSocketLink=WebSocketLink("ws://192.168.100.93:4000/");
   //final Link link=httpLink.concat(webSocketLink);
